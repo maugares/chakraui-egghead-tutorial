b23b5485c597f5f1086ce7d31f6e0fdc5e82b99b
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 17:35:49 +0100

    Add two containers next to each other (VStack)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Container, Flex, VStack } from '@chakra-ui/react'
 
 /*
 - Components have default parameters and are overriten with Style props.
@@ -8,13 +8,31 @@ import { Container, Flex } from '@chakra-ui/react'
 - These style props can be checked out on the Chakra UI website: https://chakra-ui.com/docs/styled-system/theming/theme or
 ./node_modules/@chakra-ui/styled-system/src/config/space.ts
 - design tokens have long and short forms: width (w), height (h), padding (p), margin (m), etc
+- spaces integers are 4x in pixels: 10 == 40px
+- background and backgroundColor are interchangeable
 */
 
 const IndexPage = () => (
-    <Container maxWidth="container.xl" padding={0}>
-        <Flex height="100vh" paddingY={20}>
-        </Flex>      
-    </Container>
+  <Container maxWidth='container.xl' padding={0}>
+    <Flex height='100vh' paddingY={20}>
+      <VStack
+        width='full'
+        height='full'
+        padding={10}
+        spacing={10}
+        alignItems='flex-start'
+        backgroundColor='pink'
+      />
+      <VStack
+        width='full'
+        height='full'
+        padding={10}
+        spacing={10}
+        alignItems='flex-start'
+        background='lightblue'
+      />
+    </Flex>
+  </Container>
 )
 
 export default IndexPage
