a49153a47b68f084ae830229df8f828ff1ed2c1e
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 18:19:20 +0100

    Add base-12 grid division and add other field

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,11 +1,53 @@
-import { FormControl, FormLabel, GridItem, Input, SimpleGrid } from '@chakra-ui/react'
+import {
+  FormControl,
+  FormLabel,
+  GridItem,
+  Input,
+  SimpleGrid
+} from '@chakra-ui/react'
 
 const Form = () => (
-  <SimpleGrid columns={2} columnGap={3} rowGap={6}>
-    <GridItem colSpan={1}>
+  <SimpleGrid columns={12} columnGap={3} rowGap={6}>
+    <GridItem colSpan={6}>
       <FormControl>
         <FormLabel>First Name</FormLabel>
-        <Input placeholder="John"/>
+        <Input placeholder='John' />
+      </FormControl>
+    </GridItem>
+    <GridItem colSpan={6}>
+      <FormControl>
+        <FormLabel>Last Name</FormLabel>
+        <Input placeholder='Doe' />
+      </FormControl>
+    </GridItem>
+    <GridItem colSpan={8}>
+      <FormControl>
+        <FormLabel>Address</FormLabel>
+        <Input placeholder='Hoofdstraat 1' />
+      </FormControl>
+    </GridItem>
+    <GridItem colSpan={2}>
+      <FormControl>
+        <FormLabel>nr.</FormLabel>
+        <Input placeholder='1' />
+      </FormControl>
+    </GridItem>
+    <GridItem colSpan={2}>
+      <FormControl>
+        <FormLabel>add.</FormLabel>
+        <Input placeholder='HS' />
+      </FormControl>
+    </GridItem>
+    <GridItem colSpan={8}>
+      <FormControl>
+        <FormLabel>City</FormLabel>
+        <Input placeholder='Amsterdam' />
+      </FormControl>
+    </GridItem>
+    <GridItem colSpan={4}>
+      <FormControl>
+        <FormLabel>Postcode</FormLabel>
+        <Input placeholder='1234AB' />
       </FormControl>
     </GridItem>
   </SimpleGrid>
