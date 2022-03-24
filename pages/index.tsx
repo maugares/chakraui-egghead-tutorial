4bd129aec24b0d6f7be6eed6b3c4d8d4301ba315
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 17:24:44 +0100

    Add flex component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Flex } from '@chakra-ui/react'
 
 /*
 - Components have default parameters and are overriten with Style props.
 - Chakra UI has its own implemented design system and every design system has designed tokens, which are colors, spacing units, fonts, sizes, etc. 
-- We can use style props to access those design tokens that are part of Chakra UI.
+- style props can access design tokens
 - Under the hood, these style props are compiled to CSS properties. 
-- These style props are well documented and can be checked out on the Chakra UI website: https://chakra-ui.com/docs/styled-system/theming/theme
+- These style props can be checked out on the Chakra UI website: https://chakra-ui.com/docs/styled-system/theming/theme or
+./node_modules/@chakra-ui/styled-system/src/config/space.ts
+- design tokens have long and short forms: width (w), height (h), padding (p), margin (m), etc
 */
 
 const IndexPage = () => (
-  <Container maxWidth="container.xl" padding={0}>
-  </Container>
+    <Container maxWidth="container.xl" padding={0}>
+        <Flex height="100vh" paddingY={20}>
+        </Flex>      
+    </Container>
 )
 
 export default IndexPage
