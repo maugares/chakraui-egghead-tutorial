b517e322587f56d1294179a9b38548ff6d0c6672
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 18:00:23 +0100

    Add a GridItem for the first name

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,8 +1,9 @@
-import { SimpleGrid } from '@chakra-ui/react'
+import { GridItem, SimpleGrid } from '@chakra-ui/react'
 
 const Form = () => (
   <SimpleGrid columns={2} columnGap={3} rowGap={6}>
-
+    <GridItem colSpan={1}>
+    </GridItem>
   </SimpleGrid>
 )
 
