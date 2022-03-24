064151c14829e0e00acc3fbd3aaa1834db76796b
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 17:46:42 +0100

    Add heading

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,14 +1,18 @@
-import { VStack } from '@chakra-ui/react'
+import { Heading, VStack } from '@chakra-ui/react'
 
 const Details = () => (
-    <VStack
-      width='full'
-      height='full'
-      padding={10}
-      spacing={10}
-      alignItems='flex-start'
-      background='pink'
-    />
-  )
+  <VStack
+    width='full'
+    height='full'
+    padding={10}
+    spacing={10}
+    alignItems='flex-start'
+    background='pink'
+  >
+    <Heading size='2xl'>
+      Your Details
+    </Heading>
+  </VStack>
+)
 
 export default Details
