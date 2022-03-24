cbfd0bbf984b5b36c0c1cdc45b68d87f5e8c85ee
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 18:02:42 +0100

    Add a FormControl with a FormLabel and an Input with a placeholder

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,8 +1,12 @@
-import { GridItem, SimpleGrid } from '@chakra-ui/react'
+import { FormControl, FormLabel, GridItem, Input, SimpleGrid } from '@chakra-ui/react'
 
 const Form = () => (
   <SimpleGrid columns={2} columnGap={3} rowGap={6}>
     <GridItem colSpan={1}>
+      <FormControl>
+        <FormLabel>First Name</FormLabel>
+        <Input placeholder="John"/>
+      </FormControl>
     </GridItem>
   </SimpleGrid>
 )
