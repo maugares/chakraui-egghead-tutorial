363d051d74d4b4f7b7a7675c60f30923e48d32e2
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 18:48:11 +0100

    Fix wrong spacing

--- src/Details.tsx
+++ src/Details.tsx
@@ -10,14 +10,10 @@ const Details = () => (
     alignItems='flex-start'
   >
     <VStack spacing={3} alignItems='flex-start'>
-      <Heading size='2xl'>
-        Your Details
-      </Heading>
-      <Text>
-        If you already have an account, click here to log in
-      </Text>
-      <Form />
+      <Heading size='2xl'>Your Details</Heading>
+      <Text>If you already have an account, click here to log in</Text>
     </VStack>
+    <Form />
   </VStack>
 )
 
