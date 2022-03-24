61e0fc0b13d2e83678110fa0d18cf1c0755a01df
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-24 16:21:48 +0100

    Add maxWidth and set padding to 0 for Container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import { Container } from '@chakra-ui/react'
 
-const IndexPage = () => {
-  return <Container>
+/*
+- Components have default parameters and are overriten with Style props.
+- Chakra UI has its own implemented design system and every design system has designed tokens, which are colors, spacing units, fonts, sizes, etc. 
+- We can use style props to access those design tokens that are part of Chakra UI.
+- Under the hood, these style props are compiled to CSS properties. 
+- These style props are well documented and can be checked out on the Chakra UI website: https://chakra-ui.com/docs/styled-system/theming/theme
+*/
+
+const IndexPage = () => (
+  <Container maxWidth="container.xl" padding={0}>
   </Container>
-}
+)
 
 export default IndexPage
