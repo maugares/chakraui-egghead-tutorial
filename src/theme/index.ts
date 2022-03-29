5dd2ce5572622e41801e8be394e839d34afcf3b9
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-29 09:50:10 +0200

    Use the brand theme in every Checkbox components

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,8 @@
-import { extendTheme, theme as base } from '@chakra-ui/react'
+import {
+  extendTheme,
+  theme as base,
+  withDefaultColorScheme
+} from '@chakra-ui/react'
 
 // extendTheme options: {
 //   breakpoints: '',
@@ -17,25 +21,33 @@ import { extendTheme, theme as base } from '@chakra-ui/react'
 //   transition: ''
 // }
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      50: '#f5fee5',
-      100: '#e1fbb2',
-      200: '#cdf781',
-      300: '#b8ee56',
-      400: '#a2e032',
-      500: '#8ac919',
-      600: '#71ab09',
-      700: '#578602',
-      800: '#3c5e00',
-      900: '#203300'
+// withDefaultColorScheme method allows to set a custom color scheme to all the instances of a certain components
+
+const theme = extendTheme(
+  {
+    colors: {
+      brand: {
+        50: '#f5fee5',
+        100: '#e1fbb2',
+        200: '#cdf781',
+        300: '#b8ee56',
+        400: '#a2e032',
+        500: '#8ac919',
+        600: '#71ab09',
+        700: '#578602',
+        800: '#3c5e00',
+        900: '#203300'
+      }
+    },
+    fonts: {
+      heading: `Montserrat, ${base.fonts?.heading}`,
+      body: `Inter, ${base.fonts?.body}`
     }
   },
-  fonts: {
-    heading: `Montserrat, ${base.fonts?.heading}`,
-    body: `Inter, ${base.fonts?.heading}`
-  }
-})
+  withDefaultColorScheme({
+    colorScheme: 'brand',
+    components: ['Checkbox']
+  })
+)
 
 export default theme
