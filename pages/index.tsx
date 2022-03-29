a7a74da696cea94e3262ba6960544d0b3e0e049c
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-29 08:53:47 +0200

    Add responsive padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,11 @@ import Cart from '../src/Cart/Cart'
 import Details from '../src/Details/Details'
 
 // For explanation on style props see README.md
+// padding made responsive by passing an array with different padding values for the different widht breakpoints: [<479, 480-767, >768]
 
 const IndexPage = () => (
   <Container maxWidth='container.xl' padding={0}>
-    <Flex height='100vh' paddingY={20}>
+    <Flex height='100vh' paddingY={[0, 10, 20]}>
       <Details />
       <Cart />
     </Flex>
