f77d652bec72e11d8a2f7c2ba55c4cf62e8addd7
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-29 09:28:30 +0200

    Set custom fonts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,24 @@
 import { extendTheme } from '@chakra-ui/react'
 
-const theme = extendTheme()
+// extendTheme options: {
+//   breakpoints: '',
+//   zIndices: '',
+//   radii: '',
+//   colors: '',
+//   letterSpacings: '',
+//   lineHeights: '',
+//   fontWeights: '',
+//   fonts: '',
+//   fontSizes: '',
+//   sizes: '',
+//   shadows: '',
+//   space: '',
+//   borders: '',
+//   transition: ''
+// }
+
+const theme = extendTheme({
+  fonts: {heading: 'Roboto', body: 'Arial'}
+})
 
 export default theme
