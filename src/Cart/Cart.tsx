279cb6a33dea7a97fff8afe51bfafd4d6cce3086
Muaro García Esteban <mauro.garcia.esteban@diymaxeda.com>
2022-03-29 08:41:01 +0200

    Add color mode toggle

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -8,58 +8,69 @@ import {
   AspectRatio,
   HStack,
   Stack,
-  Divider
+  Divider,
+  useColorMode,
+  Button
 } from '@chakra-ui/react'
 
-const Cart = () => (
-  <VStack
-    width='full'
-    height='full'
-    padding={10}
-    spacing={6}
-    align='flex-start'
-  >
-    <VStack spacing={3} alignItems='flex-start'>
-      <Heading size='2xl'>Your cart</Heading>
-      <Text>If price is too hard on your eyes, try changing the theme</Text>
-    </VStack>
-    <HStack
-      spacing={8}
-      alignItems='center'
+const Cart = () => {
+  const { toggleColorMode } = useColorMode()
+
+  return (
+    <VStack
       width='full'
-      justifyContent='space-between'
+      height='full'
+      padding={10}
+      spacing={6}
+      align='flex-start'
     >
-      <AspectRatio ratio={1} width='96px'>
-        <Image src='/images/skateboard.jpg' alt='Skateboard' />
-      </AspectRatio>
-      <VStack width='full' spacing={0} alignItems='flex-start'>
-        <Heading size='md'>Penny board</Heading>
-        <Text>PNYCOMP27541</Text>
+      <VStack spacing={3} alignItems='flex-start'>
+        <Heading size='2xl'>Your cart</Heading>
+        <Text>
+          If the price is too hard on your eyes,{' '}
+          <Button onClick={toggleColorMode} variant='link' colorScheme='black'>
+            try changing the theme.
+          </Button>
+        </Text>
       </VStack>
-      <Heading size='sm' textAlign='end'>
-        $119.00
-      </Heading>
-    </HStack>
-    <VStack spacing={4} alignItems='stretch' width='full'>
-      <HStack justifyContent='space-between'>
-        <Text>Subtotal</Text>
-        <Heading size='sm'>$119.00</Heading>
+      <HStack
+        spacing={8}
+        alignItems='center'
+        width='full'
+        justifyContent='space-between'
+      >
+        <AspectRatio ratio={1} width='96px'>
+          <Image src='/images/skateboard.jpg' alt='Skateboard' />
+        </AspectRatio>
+        <VStack width='full' spacing={0} alignItems='flex-start'>
+          <Heading size='md'>Penny board</Heading>
+          <Text>PNYCOMP27541</Text>
+        </VStack>
+        <Heading size='sm' textAlign='end'>
+          $119.00
+        </Heading>
       </HStack>
-      <HStack justifyContent='space-between'>
-        <Text>Shipping</Text>
-        <Heading size='sm'>$19.99</Heading>
-      </HStack>
-      <HStack justifyContent='space-between'>
-        <Text>Taxes (estimated)</Text>
-        <Heading size='sm'>$23.80</Heading>
+      <VStack spacing={4} alignItems='stretch' width='full'>
+        <HStack justifyContent='space-between'>
+          <Text>Subtotal</Text>
+          <Heading size='sm'>$119.00</Heading>
+        </HStack>
+        <HStack justifyContent='space-between'>
+          <Text>Shipping</Text>
+          <Heading size='sm'>$19.99</Heading>
+        </HStack>
+        <HStack justifyContent='space-between'>
+          <Text>Taxes (estimated)</Text>
+          <Heading size='sm'>$23.80</Heading>
+        </HStack>
+      </VStack>
+      <Divider />
+      <HStack justifyContent='space-between' width='full'>
+        <Text>Total</Text>
+        <Heading size='lg'>$162.79</Heading>
       </HStack>
     </VStack>
-    <Divider />
-    <HStack justifyContent='space-between' width='full'>
-      <Text>Total</Text>
-      <Heading size='lg'>$162.79</Heading>
-    </HStack>
-  </VStack>
-)
+  )
+}
 
 export default Cart
